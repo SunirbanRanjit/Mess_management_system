b0d30820ef58dd029b32c88e8a8ad1b492e64c60
Sunirban Ranjit <77260637+SunirbanRanjit@users.noreply.github.com>
2022-08-09 20:49:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 //const io = socket(server);
 
 
-mongoose.connect( 'mongodb://localhost:27017/test', {
+mongoose.connect( process.env.MONGO_URL || 'mongodb://localhost:27017/test', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true
